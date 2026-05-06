c529528f32dd9a638b155064754c27f16fe8d96b
Felipe Castro <146021799+castroofelipee@users.noreply.github.com>
2026-05-06 15:03:45 -0300

    Show source code reference on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ import { ProfileInfo } from '@/components/profile/ProfileInfo'
 import { LinkList } from '@/components/links/LinkList'
 import { SocialLinks } from '@/components/social/SocialLinks'
 import { WalkingPapo } from '@/components/papo/WalkingPapo'
+import { SourceCodeReference } from '@/components/source/SourceCodeReference'
 import { profile } from '@/lib/data/profile'
 import { links } from '@/lib/data/links'
 import { socials } from '@/lib/data/socials'
+import { sourceCodeLink } from '@/lib/data/source-code'
 
 export default function HomePage() {
   return (
     <FilmFrame>
       <>
         <div className="flex flex-col items-center gap-8 px-6 py-12">
+          <SourceCodeReference link={sourceCodeLink} />
+
           <div className="flex flex-col items-center gap-5">
             <ProfilePhoto
               src={profile.photoSrc}
