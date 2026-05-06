b065c3f743c7dd1df794ba99ba5ea29e1ba2961c
Felipe Castro <146021799+castroofelipee@users.noreply.github.com>
2026-05-06 15:02:57 -0300

    Add source code reference data

---
+++ lib/data/source-code.ts
@@ -0,0 +1,8 @@
+import type { LinkItem } from '../types'
+
+export const sourceCodeLink: LinkItem = {
+  id: 'source-code',
+  label: 'Source code',
+  href: 'https://github.com/castroofelipee/felipe-linktree',
+  icon: 'github',
+}
