e104b6b508ac2619111d84213ad7255198ec59ac
Felipe Castro <146021799+castroofelipee@users.noreply.github.com>
2026-07-14 21:23:18 -0300

    update profile bio

--- lib/data/profile.ts
+++ lib/data/profile.ts
@@ -2,7 +2,7 @@ import type { ProfileData } from '../types'
 
 export const profile: ProfileData = {
   name: 'Felipe R. Castro',
-  bio: 'Developer, founder of PinnSystem ',
+  bio: 'Software Engineer and builing PinnSystem. Writer at free time',
   initials: 'FC',
   photoSrc: '/ted-or-me.jpg',
   photoAlt: 'Foto de perfil de Felipe Castro',
